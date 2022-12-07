4761e365068f3a073ddf1b8be6c3c5d45cd4b502
Salman Azeem <salmanazeemkhan@gmail.com>
2022-12-07 11:17:35 +0400

    added test css at the header

--- header.php
+++ header.php
@@ -14,19 +14,23 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.12.0/css/all.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/test.css">
 </head>
+
 <body>
-<div class="stickyMain">
+    <div class="stickyMain">
         <div class="addBg">
-                <div><h2><span class="hdStyle1">UNLOCK</span> THE BEST OF YOUR CITY</h2></div>
-                <div class="addsStore">
-                    <img class="floatBubble mr10" src="images/add1.png" alt="*" />
-                    <img class="floatBubble1" src="images/add2.png" alt="*" />
-                </div>
-                <div class="addApple">
-                    <a href=""><img class="mr10" src="images/appstore.png" alt="*" /></a>
-                    <a href=""><img src="images/playstore.png" alt="*" /></a>
-                </div>
+            <div>
+                <h2><span class="hdStyle1">UNLOCK</span> THE BEST OF YOUR CITY</h2>
+            </div>
+            <div class="addsStore">
+                <img class="floatBubble mr10" src="images/add1.png" alt="*" />
+                <img class="floatBubble1" src="images/add2.png" alt="*" />
+            </div>
+            <div class="addApple">
+                <a href=""><img class="mr10" src="images/appstore.png" alt="*" /></a>
+                <a href=""><img src="images/playstore.png" alt="*" /></a>
+            </div>
         </div>
     </div>
 
@@ -43,7 +47,8 @@
                 </div>
                 <div class="col-md-4 logo"><a href=""><img src="images/logo.png" alt="" /></a></div>
                 <div class="col-md-4">
-                    <button class="modalSrchBtn desktopModSrch" data-bs-toggle="modal" data-bs-target="#srchModal"><i class="far fa-search"></i></button>
+                    <button class="modalSrchBtn desktopModSrch" data-bs-toggle="modal" data-bs-target="#srchModal"><i
+                            class="far fa-search"></i></button>
                 </div>
             </div>
         </div>
@@ -56,7 +61,9 @@
                         <nav class="navbar navbar-expand-lg">
 
                             <!-- <a class="navbar-brand" href="#">Navbar</a>-->
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"><i class="far fa-bars"></i></span>
                             </button>
                             <div class="mobileLogo"><a href=""><img src="images/logo.png" alt="" /></a></div>
@@ -64,7 +71,8 @@
                                 <span class="closeMenu"><i class="far fa-times-circle"></i></span>
                                 <ul class="navbar-nav mb-2 mb-lg-0">
                                     <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="/news-and-features.php">News &amp; Features</a>
+                                        <a class="nav-link active" aria-current="page"
+                                            href="/news-and-features.php">News &amp; Features</a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link" href="/guides.php">Guides</a>
@@ -89,7 +97,8 @@
                                 </div>
 
                             </div>
-                            <button class="modalSrchBtn responsiveModalSrch" data-bs-toggle="modal" data-bs-target="#srchModal"><i class="far fa-search"></i></button>
+                            <button class="modalSrchBtn responsiveModalSrch" data-bs-toggle="modal"
+                                data-bs-target="#srchModal"><i class="far fa-search"></i></button>
 
                         </nav>
                     </div>
