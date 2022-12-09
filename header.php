4c738607d66cd7a95ad5b1853e624030ed2c0cbd
Aqsa Ayub <ayubaqsa5@gmail.com>
2022-12-09 11:45:37 +0500

    css and header file of sami

---
+++ header.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Huntr</title>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="fonts/stylesheet.css">
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/media.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.12.0/css/all.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+</head>
+<body>
+    <div class="stickyMain">
+        <div class="addBg">
+            <div>
+                <h2><span class="hdStyle1">UNLOCK</span> THE BEST OF YOUR CITY</h2>
+            </div>
+            <div class="addsStore">
+                <img class="floatBubble mr10" src="images/add1.png" alt="*" />
+                <img class="floatBubble1" src="images/add2.png" alt="*" />
+            </div>
+            <div class="addApple">
+                <a href=""><img class="mr10" src="images/appstore.png" alt="*" /></a>
+                <a href=""><img src="images/playstore.png" alt="*" /></a>
+            </div>
+        </div>
+    </div>
+    <section class="logoBar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <!--                <a href="" class="getBtn"><img src="images/getBtn.png" alt="" /></a>-->
+                    <div class="headsocial">
+                        <a href=""><img src="images/fb.png" alt="" /></a>
+                        <a href=""><img src="images/insta.png" alt="" /></a>
+                        <a href=""><img src="images/tiktok.png" alt="" /></a>
+                    </div>
+                </div>
+                <div class="col-md-4 logo"><a href="/"><img src="images/logo.png" alt="" /></a></div>
+                <div class="col-md-4">
+                    <button class="modalSrchBtn desktopModSrch" data-bs-toggle="modal" data-bs-target="#srchModal"><i
+                            class="far fa-search"></i></button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container desktop-menu">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mainNav">
+                        <nav class="navbar navbar-expand-lg ">
+                            <!-- <a class="navbar-brand" href="#">Navbar</a>-->
+                            
+                            
+                            <div class="collapse navbar-collapse deskCentr">
+                               
+                                <ul class="navbar-nav mb-2 mb-lg-0">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" aria-current="page"
+                                            href="/news-and-features.php">News &amp; Features</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/places.php">Places</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/guides.php">Guides</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">The Huntr Membership</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">The Huntr Store</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">About</a>
+                                    </li>
+                                </ul>
+                               
+                            </div>
+                            
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+   
+    <section>
+        <div class="container mobile-menu">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mainNav">
+                        <nav class="navbar navbar-expand-lg">
+                            <!--                                <a class="navbar-brand" href="#">Navbar</a>-->
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"><i class="far fa-bars"></i></span>
+                            </button>
+                            <div class="mobileLogo"><a href="/"><img src="images/logo.png" alt="" /></a></div>
+                            <div class="collapse navbar-collapse deskCentr" id="navbarSupportedContent">
+                                <span class="closeMenu"><i class="far fa-times-circle"></i></span>
+                                <ul class="navbar-nav mb-2 mb-lg-0">
+                                <li class="dropdown nav-item">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> NEWS & FEATURES</a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Latest News</a></li>
+            <li><a href="#">Trending</a></li>
+            <li><a href="#">Food & Drink</a></li>
+            <li><a href="#">Style & Beauty</a></li>
+            <li><a href="#">Art & Culture</a></li>
+            <li><a href="#">Travel</a></li>
+          </ul>
+        </li>
+        <li class="dropdown nav-item">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Places</a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Explore & Search Page</a></li>
+            <li><a href="#">Specialty Coffee </a></li>
+            <li><a href="#">Restaurants</a></li>
+            <li><a href="#">Bars & Nightlife </a></li>
+            <li><a href="#">Art & Culture</a></li>
+            <li><a href="#">Beauty & Spa </a></li>
+            <li><a href="#">Health & Fitness  </a></li>
+            <li><a href="#">Hotels</a></li>
+            <li><a href="#">Tourist Attractions </a></li>
+            <li><a href="#">Outdoor Activities </a></li>
+            <li><a href="#">Shops</a></li>
+          </ul>
+        </li>
+        <li class="dropdown nav-item">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">GUIDES</a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Latest Guides </a></li>
+            <li><a href="#">Trending</a></li>
+            <li><a href="#">HUNTR Member Perks</a></li>
+            <li><a href="#">The HUNTR Coffee Card </a></li>
+            <li><a href="#">Restaurants</a></li>
+            <li><a href="#">Cafes</a></li>
+            <li><a href="#">Specialty Coffee</a></li>
+            <li><a href="#">Outdoor Activities</a></li>
+            <li><a href="#">Art & Culture</a></li>
+            <li><a href="#">Travel</a></li>
+            
+          </ul>
+        </li>
+        <li class="dropdown nav-item">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">THE HUNTR MEMBERSHIP</a>
+          <ul class="dropdown-menu">
+            <li><a href="#">HUNTR Member Perks</a></li>
+            <li><a href="#">HUNTR Coffee Card </a></li>
+            <li><a href="#">The HUNTR App</a></li>
+          </ul>
+        </li>
+        <li class="dropdown nav-item">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">THE HUNTR STORE</a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Buy HUNTR Membership Gift</a></li>
+            <li><a href="#">Experiences (coming soon)</a></li>
+            <li><a href="#">Gifts (coming soon)</a></li>
+          </ul>
+        </li>
+        <li class="dropdown nav-item">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ABOUT</a>
+          <ul class="dropdown-menu">
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Meet The HUNTR</a></li>
+            <li><a href="#">Terms & Conditions</a></li>
+            <li><a href="#">Editorial Policy</a></li>
+          </ul>
+        </li>
+                                    
+                                    
+                                   
+                                
+          
+                                </ul>
+                                <div class="headsocial mobSocial">
+                                    <a href=""><img src="images/fb.png" alt="" /></a>
+                                    <a href=""><img src="images/insta.png" alt="" /></a>
+                                    <a href=""><img src="images/tiktok.png" alt="" /></a>
+                                </div>
+                            </div>
+                            <button class="modalSrchBtn responsiveModalSrch" data-bs-toggle="modal" data-bs-target="#srchModal"><i class="far fa-search"></i></button>
+                        </nav>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
