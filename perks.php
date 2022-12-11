58cf7b3e0a2b06ca9f77598717eb66ba4ae3ce19
gouharDesigner <gouhar.designer@hotmail.com>
2022-12-11 19:16:13 +0500

    place, place detail, perks page created

---
+++ perks.php
@@ -0,0 +1,85 @@
+<?php include('./header.php'); ?>
+
+<!-- Inner Banner Start -->   
+<section class="inner-banner">
+  <div class="container-fluid">    
+    <div class="row top-banner">
+        <h2 class="inner-banner-text">Huntr Member perks</h2>
+        <img src="./images/news-banner.jpg" alt="*">
+    </div>
+  </div>
+</section>
+<!-- Inner Banner End -->
+
+
+
+<!-- Perks start-->
+<section class="latestNewsSection">
+  <div class="container">
+    <div id="contentScroll" class="row">
+        <div class="col-md-12">
+            <h2 class="upperCs popinsFont5 hdBtn-aling">Get inspired <a href="/news-and-features.php" class="viewMbtn">View More <i class="fal fa-long-arrow-right"></i></a></h2>
+        </div>
+        <div id="perksCarousel" class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="placesList">
+                    <figure><img src="images/place-img01.jpg" alt="*" /></figure>
+                    <div class="text">
+                        <div class="h5" href="">Buy 1 get 1 free day passes at caesars palace</div>
+                        <p>At comptoir 102, Paus, Qahwaty & more...</p>
+                        <h6>26 Places <span>0.4km <i class="far fa-map-marker-alt"></i></span></h6>
+                    </div>
+                </div>
+            </div><!-- .item-->
+            <div class="item">
+                <div class="placesList">
+                    <figure><img src="images/place-img02.jpg" alt="*" /></figure>
+                    <div class="text">
+                        <div class="h5" href="">Buy 1 get 1 free day passes at caesars palace</div>
+                        <p>At comptoir 102, Paus, Qahwaty & more...</p>
+                        <h6>26 Places <span>0.4km <i class="far fa-map-marker-alt"></i></span></h6>
+                    </div>
+                </div>
+            </div><!-- .item-->
+            <div class="item">
+                <div class="placesList">
+                    <figure><img src="images/place-img03.jpg" alt="*" /></figure>
+                    <div class="text">
+                        <div class="h5" href="">Buy 1 get 1 free day passes at caesars palace</div>
+                        <p>At comptoir 102, Paus, Qahwaty & more...</p>
+                        <h6>26 Places <span>0.4km <i class="far fa-map-marker-alt"></i></span></h6>
+                    </div>
+                </div>
+            </div><!-- .item-->
+            <div class="item">
+                <div class="placesList">
+                    <figure><img src="images/place-img01.jpg" alt="*" /></figure>
+                    <div class="text">
+                        <div class="h5" href="">Buy 1 get 1 free day passes at caesars palace</div>
+                        <p>At comptoir 102, Paus, Qahwaty & more...</p>
+                        <h6>26 Places <span>0.4km <i class="far fa-map-marker-alt"></i></span></h6>
+                    </div>
+                </div>
+            </div><!-- .item-->
+            <div class="item">
+                <div class="placesList">
+                    <figure><img src="images/place-img02.jpg" alt="*" /></figure>
+                    <div class="text">
+                        <div class="h5" href="">Buy 1 get 1 free day passes at caesars palace</div>
+                        <p>At comptoir 102, Paus, Qahwaty & more...</p>
+                        <h6>26 Places <span>0.4km <i class="far fa-map-marker-alt"></i></span></h6>
+                    </div>
+                </div>
+            </div><!-- .item-->
+        </div>    
+        
+
+    </div>
+  </div>     
+</section>
+<!-- Perks end-->
+
+
+<?php 
+   include('./footer.php'); 
+?>
