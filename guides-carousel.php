6f87827687c8a24e3aaf58b684bee81fe11e9033
Salman Azeem <salmanazeemkhan@gmail.com>
2022-12-14 13:43:44 +0400

    guides link fixed and guide detail page title

--- guides-carousel.php
+++ guides-carousel.php
@@ -1,91 +1,91 @@
 <div class="row">
-                <div class="col-md-12 guideSlide">
-                    <div id="guideCarousal" class="owl-carousel owl-theme">
-                        <div class="item">
-                            <div class="placeBlock">
-                                <div class="placeTop">
-                                    <span class="gem">26 PLACES</span>
-                                    <?php
+    <div class="col-md-12 guideSlide">
+        <div id="guideCarousal" class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="placeBlock">
+                    <div class="placeTop">
+                        <span class="gem">26 PLACES</span>
+                        <?php
                                  include('./share-button.php'); 
                                 ?>
-                                </div>
-                                <img src="./images/place1.png" class="img-fluid" alt="">
-                                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_blank')">
-                                <p class="category-title">GUIDE</p>
-                                    <h6>The places we can't stop thinkig about right now...</h6>
-                                    <p>They are just that good...</p>
-                                   
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="placeBlock">
-                                <div class="placeTop">
-                                    <span class="gem">26 PLACES</span>
-                                    <?php
-                                 include('./share-button.php'); 
-                                ?>
-                                </div>
-                                <img src="./images/place2.png" class="img-fluid" alt="">
-                                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_blank')">
-                                <p class="category-title">GUIDE</p>
-                                    <h6>The Ultimate Road Trip Guide: 12 Ways to Escape from life...</h6>
-                                    <p>They are just that good...</p>
-                                   
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="placeBlock">
-                                <div class="placeTop">
-                                    <span class="gem">26 PLACES</span>
-                                    <?php
-                                 include('./share-button.php'); 
-                                ?>
-                                </div>
-                                <img src="./images/place3.png" class="img-fluid" alt="">
-                                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_blank')">
-                                <p class="category-title">GUIDE</p>
-                                    <h6>The places we can't stop thinkig about right now...</h6>
-                                    <p>They are just that good...</p>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="placeBlock">
-                                <div class="placeTop">
-                                    <span class="gem">26 PLACES</span>
-                                    <?php
-                                 include('./share-button.php'); 
-                                ?>
-                                </div>
-                                <img src="./images/place5.png" class="img-fluid" alt="">
-                                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_blank')">
-                                <p class="category-title">GUIDE</p>
-                                    <h6>The places we can't stop thinkig about right now...</h6>
-                                    <p>They are just that good...</p>
-                                  
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="placeBlock">
-                                <div class="placeTop">
-                                    <span class="gem">26 PLACES</span>
-                                    <?php
-                                 include('./share-button.php'); 
-                                ?>
-                                </div>
-                                <img src="./images/place5.png" class="img-fluid" alt="">
-                                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_blank')">
-                                <p class="category-title">GUIDE</p>
-                                    <h6>The places we can't stop thinkig about right now...</h6>
-                                    <p>They are just that good...</p>
-                                    
-                                </div>
-                            </div>
-                        </div>
+                    </div>
+                    <img src="./images/place1.png" class="img-fluid" alt="">
+                    <div class="dBoxTxt" onclick="window.open('/guides-detail.php')">
+                        <p class="category-title">GUIDE</p>
+                        <h6>The places we can't stop thinkig about right now...</h6>
+                        <p>They are just that good...</p>
+
                     </div>
                 </div>
             </div>
+            <div class="item">
+                <div class="placeBlock">
+                    <div class="placeTop">
+                        <span class="gem">26 PLACES</span>
+                        <?php
+                                 include('./share-button.php'); 
+                                ?>
+                    </div>
+                    <img src="./images/place2.png" class="img-fluid" alt="">
+                    <div class="dBoxTxt" onclick="window.open('/guides-detail.php')">
+                        <p class="category-title">GUIDE</p>
+                        <h6>The Ultimate Road Trip Guide: 12 Ways to Escape from life...</h6>
+                        <p>They are just that good...</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="placeBlock">
+                    <div class="placeTop">
+                        <span class="gem">26 PLACES</span>
+                        <?php
+                                 include('./share-button.php'); 
+                                ?>
+                    </div>
+                    <img src="./images/place3.png" class="img-fluid" alt="">
+                    <div class="dBoxTxt" onclick="window.open('/guides-detail.php')">
+                        <p class="category-title">GUIDE</p>
+                        <h6>The places we can't stop thinkig about right now...</h6>
+                        <p>They are just that good...</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="placeBlock">
+                    <div class="placeTop">
+                        <span class="gem">26 PLACES</span>
+                        <?php
+                                 include('./share-button.php'); 
+                                ?>
+                    </div>
+                    <img src="./images/place5.png" class="img-fluid" alt="">
+                    <div class="dBoxTxt" onclick="window.open('/guides-detail.php')">
+                        <p class="category-title">GUIDE</p>
+                        <h6>The places we can't stop thinkig about right now...</h6>
+                        <p>They are just that good...</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="placeBlock">
+                    <div class="placeTop">
+                        <span class="gem">26 PLACES</span>
+                        <?php
+                                 include('./share-button.php'); 
+                                ?>
+                    </div>
+                    <img src="./images/place5.png" class="img-fluid" alt="">
+                    <div class="dBoxTxt" onclick="window.open('/guides-detail.php')">
+                        <p class="category-title">GUIDE</p>
+                        <h6>The places we can't stop thinkig about right now...</h6>
+                        <p>They are just that good...</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
