f3f5149c58ee46f2a9676e22dbcac268eb4d5901
Aqsa Ayub <ayubaqsa5@gmail.com>
2022-12-15 16:23:45 +0500

    terms & conditions

--- terms-and-conditions.php
+++ terms-and-conditions.php
@@ -10,3 +10,156 @@
   </div>
 </section>
 <!-- Inner Banner End -->
+
+<section class="contentArea">
+    <div class="container">
+      <div class="box">
+      <ol class="list-group list-group-numbered">
+            <li class="list-group-item list-heading"> Terms and Conditions
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">These Terms and Conditions (the “Terms”) apply to www.thehuntr.com (“The HUNTR” or “the Website”) which is owned and operated by Hh Media (“the Company”, “we”, “us”, “our”).</li>
+                    <li class="list-group-item">These Terms apply to all visitors to and users of the Website, whether registered or unregistered (individually “User” and collectively, “Users”).</li>
+                    <li class="list-group-item"> The definition of “User” or “Users” includes any person recognized in accordance with Clause 12 as a “Member”.</li>
+                    <li class="list-group-item"> By accessing the Website, and using any services that may be provided to you therein (collectively, the “Services”) you agree that you are not under 18 years of age (or the equivalent age your legal jurisdiction) and that you will comply with and be bound by these Terms.</li>
+                    <li class="list-group-item">We reserve the right to change these Terms or to modify any features of the Website at any time without notice to you, and you agree to be bound by such changes. All changes to these Terms, which include The HUNTR email publications as well as the Website, shall apply as soon as they are posted.</li>
+                </ol>
+            </li>
+            <li class="list-group-item list-heading"> In General….
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">We endeavour to provide you with a seamless and fun experience on The HUNTR, however sometimes technology breaks down. For this reason, we, unfortunately, cannot guarantee accessibility all the time and we will not be held liable for any losses arising as a result.</li>
+                    <li class="list-group-item">Users are advised to verify all content found within the Website before incurring any expense at all.</li>
+                    <li class="list-group-item">DSA Full Course</li>
+                </ol>
+            </li>
+            <li class="list-group-item list-heading"> In General….
+              <p>All content on The HUNTR is published by our editorial team because we like it. Any sponsored editorial is clearly marked as such. View The HUNTR’s Editorial Policy for more information.</p>
+            </li>
+            <li class="list-group-item list-heading"> Privacy Policy
+              <p>We are committed to protecting your privacy and security and have explained in detail the steps we take to do so. View The HUNTR’s Privacy Policy for more information.</p>
+            </li>
+
+            <li class="list-group-item list-heading"> General Conduct
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">You agree that you will not (and you agree not to allow any third party to) do any of the following:
+                      <ul>
+                        <li>Interfere in any way with the proper working of the Website or the Services, or otherwise, interfere in any way with other Use's enjoyment of them</li>
+                        <li>Collect any information about other Users (including names and/or email addresses) for any purpose</li>
+                        <li>Translate, reverse engineer or otherwise change any portion of the Website or the Services;</li>
+                        <li>Reformat any of the web pages that form part of the Website or the Services;</li>
+                        <li>Remove any copyright, trademark, or other proprietary rights notices contained from the Website or from within any content contained within it;</li>
+                        <li>Transmit to the Website any items of a destructive nature including but not limited to Trojan horses, defects, viruses, or worms</li>
+                        <li>Access, replicate, or otherwise utilize any portion of the Website or the Services for purposes of constructing or populating a searchable database of place, product, service, event, or online content recommendations;</li>
+                        <li>Copy, reproduce, store, distribute, publish, display, modify or in any way exploit any content offered on the Website other than for your own use;</li>
+                        <li>Use the Website or Services, intentionally or unintentionally, to violate any local, national or international law;</li>
+                        <li>Collect or store any Personal Data (as defined in our Privacy Policy) about any other Users in connection with the activities described above, or otherwise create or transmit unwanted electronic communications to other users of the Website or the Services;</li>
+                        <li>Add any offensive, improper, illegal, or libellous content to the Website.</li>
+                      </ul>
+                    </li>
+                    <li class="list-group-item">This list is not an exclusive list of the type of misuses of conduct that may result in the restriction, suspension, or termination of your access to the Website – they are examples of the types of misuse that are prohibited.</li>
+                </ol>
+            </li>
+            <li class="list-group-item list-heading">  Indemnity
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">This list is not an exclusive list of the type of misuses of conduct that may result in the restriction, suspension, or termination of your access to the Website – they are examples of the types of misuse that are prohibited. You agree to indemnify and defend the Company and its officers, directors, employees, successors, and assigns (collectively the “Indemnified Parties”) from and against all claims, suits, demands, and actions (and any resulting losses, damages, or costs incurred) brought against the Indemnified Parties as a direct or indirect consequence of (i) you accessing the Website, (ii) your use of the Services, (iii) your violation of any of these Terms, or (iv) the infringement by you, or anyone using your account or password, of any intellectual property or other rights.</li>
+                    <li class="list-group-item">This clause is not intended to exclude our liability in the unlikely event of fraud by one of our employees or death or personal injury caused by our negligence.</li>
+                </ol>
+            </li>
+            <li class="list-group-item list-heading"> Unsolicited Materials
+              <p>By communicating with The HUNTR, including submitting content and feature ideas, you grant the Company the royalty-free, unrestricted, worldwide, perpetual, irrevocable, non-exclusive, and fully sub-licensable right and license to publish, use, reproduce and feature such content in any way that the Company sees fit.</p>
+            </li>
+            <li class="list-group-item list-heading"> User’s Grant of Limited License
+              <p>We are committed to protecting your privacy and security and have explained in detail the steps we take to do so. View The HUNTR’s Privacy Policy for more information.</p>
+            </li>
+
+            <li class="list-group-item list-heading">  Liability for Disputes Between Users of The HUNTR and Linked Websites
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item"> We are not responsible for any website linked to The HUNTR (whether linked by affiliate program or otherwise) or for what happens to you as a result of using one. We cannot intervene in any disputes between you and such website or their users. Should you elect to engage in correspondence or commercial transactions with any third parties in connection with your use of the Website or the Services, you do so at your own risk. The Company assumes no responsibility and will have no liability, for any such activity.</li>
+                    <li class="list-group-item">You agree that you will not hold us liable for any losses, costs, claims, demands, or damages of any kind or nature arising out of or in any way connected to your reliance on any content, goods, or services found on any website linked to from The HUNTR.</li>
+                </ol>
+            </li>
+            <li class="list-group-item list-heading"> Viruses
+              <p>We can accept no responsibility for any losses arising from an attack by a virus, Trojan, worm, or other device having a negative impact on the use of your system or unlawfully giving third party access to your data, or transmitting such data to a third party.</p>
+            </li>
+            <li class="list-group-item list-heading">  Competitions / Promotions
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">The HUNTR often runs competitions and promotions. These are designed for the sole benefit of the Users of the Website.</li>
+                    <li class="list-group-item"> Users who have more than one account with The HUNTR – whether for the purpose of increasing their chances of winning a competition or otherwise – are subject to immediate disqualification from any competition or promotion.</li>
+                    <li class="list-group-item">The Company, at its sole discretion, may suspend or revoke the website access of any User.</li>
+                    <li class="list-group-item">Users agree to release the Company and its agents, advertisers, sponsors, or promotional partners, from all liability arising from participation in any competition or promotion located on or accessed through, the Website.</li>
+                    <li class="list-group-item">The Company is not responsible for technical, hardware, or software failures of any kind, lost or unavailable network connections, or failed, incomplete, garbled, or delayed computer transmissions that may limit a User’s ability to participate in a competition or promotion.</li>
+                    <li class="list-group-item">Each competition and promotion comes with its own specific rules and small print – this is always clearly displayed on the page of the Website on which the competition is featured. Users agree to be bound by such rules at all times.</li>
+                    <li class="list-group-item">A User who violates any of the rules or guidelines for behavior published on the Website is subject to immediate disqualification and revocation of their registration. The Company is under no obligation to award any prize to any User who violates a published rule or guideline.</li>
+                    <li class="list-group-item">The Company reserves the right to cancel, terminate or alter any competition or promotion (or the rules thereof) at any time and without prior notification.</li>
+                    
+                </ol>
+            </li>
+
+            <li class="list-group-item list-heading">  Membership
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">The Services and some elements of the Website are not available to the general public. Specifically, The HUNTR operates with a metered paywall, and certain pages of the website are only accessible to users whom we have allowed access after they have become a HUNTR Member (“Members”) by agreeing to pay to us a membership fee payable via our payment processor, Stripe.com.</li>
+                    <li class="list-group-item">Monthly and Annual Subscriptions are collectively referred to as “Subscriptions”</li>
+                    <li class="list-group-item">Members pay for unrestricted access to TheHUNTR.com.</li>
+                    <li class="list-group-item">In exchange for paying their Subscriptions to us, Members have full access to all content stored within the Website. We also sometimes arrange events and/or offers for Members offered and arranged by a variety of UAE-based restaurants, bars, event organizers, and businesses who choose to partner with Us (herein known as our “Partners”).</li>
+                    <li class="list-group-item">We act merely as promoters of any exclusive offers and opportunities brought to Us by our Partners, and so cannot be held liable in any way for the failure of one or all of our Partners to deliver what has been offered to a HUNTR Member within any part of The HUNTR.</li>
+                    <li class="list-group-item">Prices for HUNTR Subscriptions – which are currently 30AED monthly or 330AED annually – may be changed at any time at Our sole discretion (though we have, and will never, knowingly increase the rate paid by an existing member once they have secured it).</li>
+                    <li class="list-group-item">No Member may have more than one Account. No Member may open an alternative account if their Subscription is suspended or terminated for any reason.</li>
+                    <li class="list-group-item">Members may not share or give access to their login, subscription, or membership with any other company or individual. The membership and its benefits are only for the named person on the account.</li>
+                    <li class="list-group-item">Members may not sell, trade, or transfer an Account to another person, nor accept or use an Account that does not belong to them.</li>
+                    <li class="list-group-item">You must pay any subscription fees in accordance with the applicable subscription terms and must provide The HUNTR, where applicable, with all necessary information to enable us to receive such payment. You must ensure that such information is current and correct at all times.</li>
+                    <li class="list-group-item"> If your membership includes a free trial period, note that your membership fee will automatically be charged and the membership will start once that trial period has passed. It is your responsibility to cancel the membership within the trial period if you do not want to be charged and continue with it.</li>
+                    
+                </ol>
+            </li>
+            <li class="list-group-item list-heading">  Termination of Account
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">Members who pay a Monthly Subscription (Monthly Members) are expressly requesting delivery of the Services on a monthly basis.</li>
+                    <li class="list-group-item">Monthly Members may terminate their Monthly Subscription at any time without cause, penalty, or explanation by simply electing to so within their account settings.</li>
+                    <li class="list-group-item">Members who pay an Annual Subscription (Annual Members) are expressly requesting delivery of the Services on an annual basis.</li>
+                    <li class="list-group-item">Annual Members may terminate their Annual Subscription at any time without cause, penalty, or explanation by simply electing to so within their account settings. Doing so means that they will not be billed for any further subscription terms and their membership will expire.</li>
+                    <li class="list-group-item">We cannot, and do not, issue pro-rata reimbursements on Annual Subscriptions.</li>
+                    <li class="list-group-item">We reserve the right to cancel the Subscription of any Member, and withhold their access to Our Services, for any reason</li>
+                    <li class="list-group-item">We cannot, and do not, issue any refunds for any Subscriptions or Memberships.</li>
+                </ol>
+            </li>
+            <li class="list-group-item list-heading"> Copyright Infringement
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">The HUNTR does not ever want to infringe the rights of any individuals or organizations whose content has been placed on the Website.</li>
+                    <li class="list-group-item">If you believe that content you own has been added to or used on the Website in a way that violates your copyright or other intellectual property rights, please contact hello@thehuntr.com with the following information, so that we can remove it:
+                    <ul>
+                        <li>A description of which intellectual property has been infringed, what it is, and where on the Website it can be found</li>
+                        <li>Your contact details</li>
+                        <li>The signature of the person authorized to act on behalf of the owner of the copyright or other intellectual property interest;</li>
+                        <li>A clear statement that you, in good faith, believe that the disputed use is not authorized and that the above information in your notice is accurate and that you are the copyright or intellectual property owner or authorized to act on the copyright or intellectual property owner’s behalf and have proof of such authorization and title to such rights.</li>
+                        
+                      </ul>
+                    </li>
+                </ol>
+            </li>
+            <li class="list-group-item list-heading">  Ownership
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">The Website and the Services (including, but not limited to, all content contained therein) are protected by copyright as collective works or compilations. The Company and its licensors are the sole and exclusive owners of all right, title, and interest in and to the Website (including all content found within it) and the Services.</li>
+                    <li class="list-group-item">You agree that you will not modify, reproduce, distribute, create derivative works of, publicly display or in any way exploit, any of the content, software, and/or materials available on the Website or via the Services, in whole or in part, except as expressly permitted by the Company.</li>
+                </ol>
+            </li>
+            <li class="list-group-item list-heading"> Non-Waiver of Rights
+              <p>Our failure to act in a particular circumstance does not waive our right to act with respect to that or other circumstances. We shall be excused for any problem due to a factor beyond our control.</p>
+            </li>
+            <li class="list-group-item list-heading"> Governing Laws
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">These Terms are governed by and construed in accordance with UAE law and you irrevocably agree that the courts of the UAE will have exclusive jurisdiction over any dispute arise out of or in connection with these Terms and our Website. Irrespective of the Country in which you live or are currently based, you agree to comply with all local laws in relation to the use of the Website.</li>
+            </li>
+            <p>If you have any questions or concerns in relation to these Terms please contact us at: hello@thehuntr.com</p>
+            
+        </ol>
+
+  
+  
+     
+      </div>  
+    </div>
+
+</section>
+
+<?php 
+   include('./footer.php'); 
+?>
