d8d67b61779137badf92c9e649102957c946fa53
Salman Azeem <salmanazeemkhan@gmail.com>
2022-12-15 18:52:49 +0400

    guide carousel updated.

--- guides-carousel.php
+++ guides-carousel.php
@@ -1,73 +1,75 @@
-<div class="row mobile">
-    <div class="col-md-12 guideSlide">
-        <div id="guideCarousal" class="owl-carousel owl-theme">
-            <div class="item">
-                <div class="placeBlock">
-                    <div class="placeTop">
-                        <span class="gem">26 PLACES</span>
-                    </div>
-                    <img src="./images/place1.png" class="img-fluid" alt="">
-                    <div class="dBoxTxt" onclick="window.open('/guides-detail.php','_self')">
-                        <p class="category-title">GUIDE</p>
-                        <h6>The places we can't stop thinkig about right now...</h6>
-                        <p>They are just that good...</p>
+<div class="mobile">
+    <div class="row">
+        <div class="col-md-12 guideSlide">
+            <div id="guideCarousal" class="owl-carousel owl-theme">
+                <div class="item">
+                    <div class="placeBlock">
+                        <div class="placeTop">
+                            <span class="gem">26 PLACES</span>
+                        </div>
+                        <img src="./images/place1.png" class="img-fluid" alt="">
+                        <div class="dBoxTxt" onclick="window.open('/guides-detail.php','_self')">
+                            <p class="category-title">GUIDE</p>
+                            <h6>The places we can't stop thinkig about right now...</h6>
+                            <p>They are just that good...</p>
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="item">
-                <div class="placeBlock">
-                    <div class="placeTop">
-                        <span class="gem">26 PLACES</span>
-                    </div>
-                    <img src="./images/place2.png" class="img-fluid" alt="">
-                    <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
-                        <p class="category-title">GUIDE</p>
-                        <h6>The Ultimate Road Trip Guide: 12 Ways to Escape from life...</h6>
-                        <p>They are just that good...</p>
+                <div class="item">
+                    <div class="placeBlock">
+                        <div class="placeTop">
+                            <span class="gem">26 PLACES</span>
+                        </div>
+                        <img src="./images/place2.png" class="img-fluid" alt="">
+                        <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                            <p class="category-title">GUIDE</p>
+                            <h6>The Ultimate Road Trip Guide: 12 Ways to Escape from life...</h6>
+                            <p>They are just that good...</p>
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="item">
-                <div class="placeBlock">
-                    <div class="placeTop">
-                        <span class="gem">26 PLACES</span>
-                    </div>
-                    <img src="./images/place3.png" class="img-fluid" alt="">
-                    <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
-                        <p class="category-title">GUIDE</p>
-                        <h6>The places we can't stop thinkig about right now...</h6>
-                        <p>They are just that good...</p>
+                <div class="item">
+                    <div class="placeBlock">
+                        <div class="placeTop">
+                            <span class="gem">26 PLACES</span>
+                        </div>
+                        <img src="./images/place3.png" class="img-fluid" alt="">
+                        <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                            <p class="category-title">GUIDE</p>
+                            <h6>The places we can't stop thinkig about right now...</h6>
+                            <p>They are just that good...</p>
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="item">
-                <div class="placeBlock">
-                    <div class="placeTop">
-                        <span class="gem">26 PLACES</span>
-                    </div>
-                    <img src="./images/place5.png" class="img-fluid" alt="">
-                    <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
-                        <p class="category-title">GUIDE</p>
-                        <h6>The places we can't stop thinkig about right now...</h6>
-                        <p>They are just that good...</p>
+                <div class="item">
+                    <div class="placeBlock">
+                        <div class="placeTop">
+                            <span class="gem">26 PLACES</span>
+                        </div>
+                        <img src="./images/place5.png" class="img-fluid" alt="">
+                        <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                            <p class="category-title">GUIDE</p>
+                            <h6>The places we can't stop thinkig about right now...</h6>
+                            <p>They are just that good...</p>
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="item">
-                <div class="placeBlock">
-                    <div class="placeTop">
-                        <span class="gem">26 PLACES</span>
-                    </div>
-                    <img src="./images/place5.png" class="img-fluid" alt="">
-                    <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
-                        <p class="category-title">GUIDE</p>
-                        <h6>The places we can't stop thinkig about right now...</h6>
-                        <p>They are just that good...</p>
+                <div class="item">
+                    <div class="placeBlock">
+                        <div class="placeTop">
+                            <span class="gem">26 PLACES</span>
+                        </div>
+                        <img src="./images/place5.png" class="img-fluid" alt="">
+                        <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                            <p class="category-title">GUIDE</p>
+                            <h6>The places we can't stop thinkig about right now...</h6>
+                            <p>They are just that good...</p>
 
+                        </div>
                     </div>
                 </div>
             </div>
@@ -75,92 +77,94 @@
     </div>
 </div>
 
-<div class="row desktop">
+<div class="desktop">
+    <div class="row">
 
-    <div class="col-md-4">
-        <div class="placeBlock">
-            <div class="placeTop">
-                <span class="gem">26 PLACES</span>
-            </div>
-            <img src="./images/place5.png" class="img-fluid" alt="">
-            <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
-                <p class="category-title">GUIDE</p>
-                <h6>The places we can't stop thinkig about right now...</h6>
-                <p>They are just that good...</p>
+        <div class="col-md-4">
+            <div class="placeBlock">
+                <div class="placeTop">
+                    <span class="gem">26 PLACES</span>
+                </div>
+                <img src="./images/place5.png" class="img-fluid" alt="">
+                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                    <p class="category-title">GUIDE</p>
+                    <h6>The places we can't stop thinkig about right now...</h6>
+                    <p>They are just that good...</p>
 
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-4">
-        <div class="placeBlock">
-            <div class="placeTop">
-                <span class="gem">26 PLACES</span>
-            </div>
-            <img src="./images/place5.png" class="img-fluid" alt="">
-            <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
-                <p class="category-title">GUIDE</p>
-                <h6>The places we can't stop thinkig about right now...</h6>
-                <p>They are just that good...</p>
+        <div class="col-md-4">
+            <div class="placeBlock">
+                <div class="placeTop">
+                    <span class="gem">26 PLACES</span>
+                </div>
+                <img src="./images/place5.png" class="img-fluid" alt="">
+                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                    <p class="category-title">GUIDE</p>
+                    <h6>The places we can't stop thinkig about right now...</h6>
+                    <p>They are just that good...</p>
 
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-4">
-        <div class="placeBlock">
-            <div class="placeTop">
-                <span class="gem">26 PLACES</span>
-            </div>
-            <img src="./images/place5.png" class="img-fluid" alt="">
-            <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
-                <p class="category-title">GUIDE</p>
-                <h6>The places we can't stop thinkig about right now...</h6>
-                <p>They are just that good...</p>
+        <div class="col-md-4">
+            <div class="placeBlock">
+                <div class="placeTop">
+                    <span class="gem">26 PLACES</span>
+                </div>
+                <img src="./images/place5.png" class="img-fluid" alt="">
+                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                    <p class="category-title">GUIDE</p>
+                    <h6>The places we can't stop thinkig about right now...</h6>
+                    <p>They are just that good...</p>
 
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-4">
-        <div class="placeBlock">
-            <div class="placeTop">
-                <span class="gem">26 PLACES</span>
-            </div>
-            <img src="./images/place5.png" class="img-fluid" alt="">
-            <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
-                <p class="category-title">GUIDE</p>
-                <h6>The places we can't stop thinkig about right now...</h6>
-                <p>They are just that good...</p>
+        <div class="col-md-4">
+            <div class="placeBlock">
+                <div class="placeTop">
+                    <span class="gem">26 PLACES</span>
+                </div>
+                <img src="./images/place5.png" class="img-fluid" alt="">
+                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                    <p class="category-title">GUIDE</p>
+                    <h6>The places we can't stop thinkig about right now...</h6>
+                    <p>They are just that good...</p>
 
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-4">
-        <div class="placeBlock">
-            <div class="placeTop">
-                <span class="gem">26 PLACES</span>
-            </div>
-            <img src="./images/place5.png" class="img-fluid" alt="">
-            <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
-                <p class="category-title">GUIDE</p>
-                <h6>The places we can't stop thinkig about right now...</h6>
-                <p>They are just that good...</p>
+        <div class="col-md-4">
+            <div class="placeBlock">
+                <div class="placeTop">
+                    <span class="gem">26 PLACES</span>
+                </div>
+                <img src="./images/place5.png" class="img-fluid" alt="">
+                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                    <p class="category-title">GUIDE</p>
+                    <h6>The places we can't stop thinkig about right now...</h6>
+                    <p>They are just that good...</p>
 
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-4">
-        <div class="placeBlock">
-            <div class="placeTop">
-                <span class="gem">26 PLACES</span>
-            </div>
-            <img src="./images/place5.png" class="img-fluid" alt="">
-            <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
-                <p class="category-title">GUIDE</p>
-                <h6>The places we can't stop thinkig about right now...</h6>
-                <p>They are just that good...</p>
+        <div class="col-md-4">
+            <div class="placeBlock">
+                <div class="placeTop">
+                    <span class="gem">26 PLACES</span>
+                </div>
+                <img src="./images/place5.png" class="img-fluid" alt="">
+                <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                    <p class="category-title">GUIDE</p>
+                    <h6>The places we can't stop thinkig about right now...</h6>
+                    <p>They are just that good...</p>
 
+                </div>
             </div>
         </div>
-    </div>
 
-            
+
+    </div>
 </div>
