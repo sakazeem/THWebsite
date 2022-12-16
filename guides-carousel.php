68344cc1b67dcaf6164e77fb9a1ca0f074a36c20
Aqsa Ayub <ayubaqsa5@gmail.com>
2022-12-16 11:45:42 +0500

    9 items

--- guides-carousel.php
+++ guides-carousel.php
@@ -166,6 +166,49 @@
                     </div>
         </div>
 
+        <div class="col-md-4">
+        <div class="placeBlock">
+                        <div class="placeTop">
+                            <span class="gem">26 PLACES</span>
+                        </div>
+                        <img src="./images/place1.png" class="img-fluid" alt="">
+                        <div class="dBoxTxt" onclick="window.open('/guides-detail.php','_self')">
+                            <p class="category-title">GUIDE</p>
+                            <h6>The places we can't stop thinkig about right now...</h6>
+                            <p>They are just that good...</p>
+
+                        </div>
+                    </div>
+        </div>
+        <div class="col-md-4">
+        <div class="placeBlock">
+                        <div class="placeTop">
+                            <span class="gem">26 PLACES</span>
+                        </div>
+                        <img src="./images/place2.png" class="img-fluid" alt="">
+                        <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                            <p class="category-title">GUIDE</p>
+                            <h6>The Ultimate Road Trip Guide: 12 Ways to Escape from life...</h6>
+                            <p>They are just that good...</p>
+
+                        </div>
+                    </div>
+        </div>
+        <div class="col-md-4">
+        <div class="placeBlock">
+                        <div class="placeTop">
+                            <span class="gem">26 PLACES</span>
+                        </div>
+                        <img src="./images/place3.png" class="img-fluid" alt="">
+                        <div class="dBoxTxt" onclick="window.open('/guides-detail.php', '_self')">
+                            <p class="category-title">GUIDE</p>
+                            <h6>The places we can't stop thinkig about right now...</h6>
+                            <p>They are just that good...</p>
+
+                        </div>
+                    </div>
+        </div>
+
 
     </div>
 </div>
--- places-carousel.php
+++ places-carousel.php
@@ -284,5 +284,84 @@
             </div>
             </div>
         </div>
+
+        <div class="col-md-4">
+        <div class="placeBlock">
+            <div class="placeTop">
+            
+            <div>&nbsp;</div> 
+         
+            <img src="./images/icon-perk.png" class="emoji" alt="">
+            <img src="./images/icon-coffee.png" class="emoji-coffee" alt="">
+                 
+            </div>
+            <div class="placeMiddle">
+            <h5>RESERVATIONS RECOMMENDED</h5>
+            <h5>LICENSED</h5>
+            <h5>OUTSIDE SITTING</h5>
+            </div>
+            
+            <img src="./images/place5.png" class="img-fluid" alt="">
+            
+            <div class="dBoxTxt" onclick="window.open('/places-detail.php','_self')">
+                <h5>RESTURANT & BAR IN DUBAI</h5>
+                <h6>Demo sub heading will be here</h6>
+                <p>Demo text will be here</p>
+                
+            </div>
+        </div>
+        </div>
+
+        <div class="col-md-4">
+        <div class="placeBlock">
+            <div class="placeTop">
+            <img src="./images/icon-perk.png" class="emoji" alt="">
+            <img src="./images/icon-coffee.png" class="emoji-coffee" alt="">
+
+            <div>&nbsp;</div>    
+            </div>
+            <div class="placeMiddle">
+            <h5>RESERVATIONS RECOMMENDED</h5>
+            <h5>LICENSED</h5>
+            <h5>OUTSIDE SITTING</h5>
+            </div>
+            
+            <img src="./images/place4.png" class="img-fluid" alt="">
+            
+            <div class="dBoxTxt" onclick="window.open('/places-detail.php','_self')">
+                <h5>RESTURANT & BAR IN DUBAI</h5>
+                <h6>Demo sub heading will be here</h6>
+                <p>Demo text will be here</p>
+                
+            </div>
+        </div>
+        </div>
+        <div class="col-md-4">
+             <div class="placeBlock">
+            
+            <div class="placeTop">
+           
+            <img src="./images/icon-perk.png" class="emoji" alt="">
+            <img src="./images/icon-coffee.png" class="emoji-coffee" alt="">
+            <div>&nbsp;</div>
+            </div>
+            <div class="placeMiddle">
+            <h5>RESERVATIONS RECOMMENDED</h5>
+            <h5>LICENSED</h5>
+            <h5>OUTSIDE SITTING</h5>
+            
+            </div>
+            
+            <img src="./images/place2.png" class="img-fluid" alt="">
+            
+            <div class="dBoxTxt" onclick="window.open('/places-detail.php','_self')">
+                <h5>RESTURANT & BAR IN DUBAI</h5>
+                <h6>Demo sub heading will be here</h6>
+                <p>Demo text will be here</p>
+                
+            </div>
+        
+            </div>
+        </div>
     </div>
 </div>
